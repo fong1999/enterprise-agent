65d11c64502608325002d703566a8fec3a256011
Fong <fong1988@Fongs-MacBook-Pro.local>
2025-02-23 21:56:15 -0500

    Code cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { ContentFeedComponent } from './components/content-feed/content-feed.component';
 import { RightPanelComponent } from './components/right-panel/right-panel.component';
-import { ChatComponent } from './components/chat/chat.component';
+import { ChatbotContainerComponent } from './components/chatbot-container/chatbot-container.component';
 
 export const routes: Routes = [
   { path: '', component: ContentFeedComponent }, // Default Home Page
-  { path: 'chat', component: ChatComponent },
+  { path: 'chat', component: ChatbotContainerComponent },
   { path: 'feed', component: ContentFeedComponent },
   { path: 'right-panel', component: RightPanelComponent }
 ];
--- src/app/components/chat/chat.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-chat',
-  standalone: true,
-  templateUrl: './chatbot-container.component.html',
-  styleUrls: ['./chatbot-container.component.scss']
-})
-export class ChatComponent { }
